3e688a0864cfe31309756e7c970fac69bd9d45c6
Yang Zhu <zhuyang@sidechef.com>
2024-10-06 22:01:53 +0800

    Add cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
+  video: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'report',
+    reportFilename: 'test-report',
+    // videoOnFailOnly: true,
+    code: false
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:run', async (details) => {
+        await beforeRunHook(details);
+      })
+      on('after:run', async () => {
+        await afterRunHook()
+      })
     },
     specPattern: 'tests/**/*.cy.js',
     supportFile: false,
--- pages/kong.js
+++ pages/kong.js
@@ -1,5 +1,25 @@
+import 'cypress-mochawesome-reporter/register'
+
 /*
   Base Page for Kong project
 */
 
-export class KongPage {}
+export class KongPage {
+
+  constructor() {
+
+    // https://github.com/cypress-io/cypress/issues/2554#issuecomment-442334752
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false
+    })
+
+    Cypress.on('after:run', (test, runnable) => {
+      const screenshot = `report/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`
+      addContext({ test }, screenshot)
+      const video = `report/videos/${Cypress.spec.name}.mp4`
+      addContext({ test }, video)
+    })
+
+  }
+
+}
